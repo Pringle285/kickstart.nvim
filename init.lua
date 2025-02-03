099907896772952015877fca6753ca212ad3a80f
Pringle285 <rhys.male@blueyonder.co.uk>
2025-02-03 22:37:00 +0000

    Added cmake to init file

--- init.lua
+++ init.lua
@@ -550,7 +550,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        cmake = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
